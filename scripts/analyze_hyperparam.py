2d6c71bf4d2f669708960236ef81e77935adb7ef
FrancescoZanella <francescozanella00@gmail.com>
2023-11-28 10:44:09 +0100

    fixed

--- scripts/analyze_hyperparam.py
+++ scripts/analyze_hyperparam.py
@@ -1,4 +1,4 @@
-#import plotly.express as px
+import plotly.express as px
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
