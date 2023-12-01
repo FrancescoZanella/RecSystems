f583db01f0a5cb991e12d75e5dda40206058f267
Federico Ciliberto <109111485+FedericoCiliberto@users.noreply.github.com>
2023-12-01 21:58:39 +0100

    Update script_output_to_csv_IALS.py

--- scripts/script_output_to_csv_IALS.py
+++ scripts/script_output_to_csv_IALS.py
@@ -5,7 +5,7 @@ def function(input_path, output_path):
     with open(input_path, 'r') as file:
         content = file.read()
 
-    blocks = re.findall(r'\[I.*?Trial (\d+).*?value: (.*?). Best.*?\n([\s\S]*?)(?=\[I|$)', content)
+    blocks = re.findall(r'\[I.*?Trial (\d+).*?value: (.*?)(?=\[I|$)([\s\S]*?)(?=\[I|$)', content)
 
     with open(output_path, 'w', newline='') as csvfile:
         fieldnames = ['trial_id', 'accuracy', 'num_factors', 'confidence_scaling', 'alpha', 'epsilon', 'reg']
@@ -14,16 +14,19 @@ def function(input_path, output_path):
 
         for block in blocks:
             trial_id, accuracy, rest_of_block = block
-            params = re.search(r"parameters: \{(.*?)\}", rest_of_block).group(1)
-            params_dict = dict(map(str.strip, param.split(':')) for param in params.split(','))
+            params_match = re.search(r"parameters: \{(.*?)\}", rest_of_block)
 
-            writer.writerow({
-                'trial_id': trial_id,
-                'accuracy': accuracy,
-                'num_factors': params_dict['num_factors'],
-                'confidence_scaling': params_dict['confidence_scaling'],
-                'alpha': params_dict['alpha'],
-                'epsilon': params_dict['epsilon'],
-                'reg': params_dict['reg']
-            })
+            if params_match:
+                params = params_match.group(1)
+                params_dict = dict(map(str.strip, param.split(':')) for param in params.split(','))
+
+                writer.writerow({
+                    'trial_id': trial_id,
+                    'accuracy': accuracy,
+                    'num_factors': params_dict.get('num_factors', ''),
+                    'confidence_scaling': params_dict.get('confidence_scaling', ''),
+                    'alpha': params_dict.get('alpha', ''),
+                    'epsilon': params_dict.get('epsilon', ''),
+                    'reg': params_dict.get('reg', '')
+                })
 
