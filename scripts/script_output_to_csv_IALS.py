64b408c0a8701a62c5bcf960b2e872134fa7e595
Federico Ciliberto <109111485+FedericoCiliberto@users.noreply.github.com>
2023-12-01 22:01:40 +0100

    Update script_output_to_csv_IALS.py

--- scripts/script_output_to_csv_IALS.py
+++ scripts/script_output_to_csv_IALS.py
@@ -14,19 +14,16 @@ def function(input_path, output_path):
 
         for block in blocks:
             trial_id, accuracy, rest_of_block = block
-            params_match = re.search(r"parameters: \{(.*?)\}", rest_of_block)
+            params_match = re.search(r"'num_factors': (\d+).*?'confidence_scaling': '(.*?)'.*?'alpha': ([\d.]+).*?'epsilon': ([\d.]+).*?'reg': ([\d.e+-]+)", rest_of_block)
 
             if params_match:
-                params = params_match.group(1)
-                params_dict = dict(map(str.strip, param.split(':')) for param in params.split(','))
-
                 writer.writerow({
                     'trial_id': trial_id,
                     'accuracy': accuracy,
-                    'num_factors': params_dict.get('num_factors', ''),
-                    'confidence_scaling': params_dict.get('confidence_scaling', ''),
-                    'alpha': params_dict.get('alpha', ''),
-                    'epsilon': params_dict.get('epsilon', ''),
-                    'reg': params_dict.get('reg', '')
+                    'num_factors': params_match.group(1),
+                    'confidence_scaling': params_match.group(2),
+                    'alpha': params_match.group(3),
+                    'epsilon': params_match.group(4),
+                    'reg': params_match.group(5)
                 })
 
