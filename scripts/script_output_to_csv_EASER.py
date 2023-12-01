659097a8ddda8ff1ca4c5d46272c6d22066e545f
Federico Ciliberto <109111485+FedericoCiliberto@users.noreply.github.com>
2023-12-02 00:51:26 +0100

    Update script_output_to_csv_EASER.py

--- scripts/script_output_to_csv_EASER.py
+++ scripts/script_output_to_csv_EASER.py
@@ -1,4 +1,5 @@
 import re
+import ast
 import pandas as pd
 
 def function(log_file_path, output_csv_path):
@@ -10,8 +11,14 @@ def function(log_file_path, output_csv_path):
     pattern = re.compile(r"\{([^}]+)\}")
     matches = pattern.findall(log_text)
 
-    # Create a list of dictionaries
-    data = [eval(match) for match in matches]
+    # Create a list of dictionaries using ast.literal_eval
+    data = []
+    for match in matches:
+        # Remove line breaks and whitespaces
+        clean_match = match.replace('\n', '').replace(' ', '')
+        # Evaluate the cleaned string
+        evaluated_dict = ast.literal_eval(clean_match)
+        data.append(evaluated_dict)
 
     # Create a Pandas DataFrame
     df = pd.DataFrame(data)
