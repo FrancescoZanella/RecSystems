b5b001b7a2b665cf921860955db019b069356fbc
FrancescoZanella <francescozanella00@gmail.com>
2023-12-06 12:50:09 +0100

    k_fold added globals

--- scripts/script_Kfold_evaluation.py
+++ scripts/script_Kfold_evaluation.py
@@ -77,11 +77,11 @@ def Kfold_hybrid(URM_all, models_in_hybrid, dict_of_dict_parameters,model_hybrid
         # train every model composing the hybrid
         trained_recommenders = []
         for model in models_in_hybrid:
-            recommender = model(URM_train)
+            recommender = globals()[model](URM_train)
             recommender.fit(**(dict_of_dict_parameters[model]))
             trained_recommenders.append(recommender)
         
-        
+
         # extract the topK value for the hybrid matrix
         selectTopK = False
         topK = 0
