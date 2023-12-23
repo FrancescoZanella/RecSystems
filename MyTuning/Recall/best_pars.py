895362ee6f154793baa316835d66a44885960cf6
Federico Ciliberto <109111485+FedericoCiliberto@users.noreply.github.com>
2023-12-23 16:39:07 +0100

    Add files via upload

---
+++ MyTuning/Recall/best_pars.py
@@ -0,0 +1,45 @@
+best_pars_dict = {
+  'SLIMElasticNetRecommender': {
+      'topK': 7223, 
+      'l1_ratio': 0.06907809571101771, 
+      'alpha': 0.001247929408227769
+    },
+  'RP3betaRecommender': {
+    'topK':21,
+    'alpha' : 0.4029792567152519,
+    'beta' : 0.19680626151757433
+  },
+  'EASE_R_Recommender': {
+      'topK': 17599, 
+      'l2_norm': 94.71018506892186
+    },
+  'P3alphaRecommender': {
+    'topK': 70,
+    'alpha': 0.31624473413346754
+  },
+  'ItemKNNCFRecommender': {
+    'similarity': "tversky",
+    'topK': 36,
+    'shrink': 29,
+    'tversky_alpha': 1.741341923073262,
+    'tversky_beta': 2.9148336121214324,
+  },
+  'SLIM_BPR_Cython': {
+    'epochs': 445,
+    'lambda_i':0.00357814405695588,
+    'lambda_j':0.0000517456087547006,
+    'learning_rate' : 0.0110299013052305,
+    'topK':18
+  },
+  'IALSRecommender': {
+    'num_factors': 237,
+    'confidence_scaling': 'log',
+    'alpha': 0.8518168781047342,
+    'epsilon': 0.015416759470287537,
+    'reg': 0.002312451046658324
+  },
+  'TopPopRecommenderUnseen':{
+  },
+  'TopPop':{
+  }
+}
