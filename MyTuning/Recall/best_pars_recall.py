9baffaae1c0e676009f73b999ddb8d907f72d39f
Federico Ciliberto <109111485+FedericoCiliberto@users.noreply.github.com>
2023-12-23 16:48:52 +0100

    Update and rename best_pars.py to best_pars_recall.py

---
+++ MyTuning/Recall/best_pars_recall.py
@@ -0,0 +1,30 @@
+best_pars_recall_dict = {
+  'SLIMElasticNetRecommender': {
+    'topK': 9940, 
+    'l1_ratio': 0.05284130389391463, 
+    'alpha': 0.001445383733256085
+  },
+  'RP3betaRecommender': {
+    'topK':55,
+    'alpha': 0.251168127955914,
+    'beta': 0.151393522942823,
+    'normalize_similarity':True,
+  },
+  'EASE_R_Recommender': {
+    'topK': 1339, 
+    'l2_norm': 227.09374680918643
+  },
+  'P3alphaRecommender': {
+    'topK': 143,
+    'alpha': 0.471097731898723,
+    'normalize_similarity': True,
+  },
+  'ItemKNNCFRecommender': {
+    'similarity': "tversky",
+    'topK': 18,
+    'shrink': 47,
+    'tversky_alpha': 0.508601988213229,
+    'tversky_beta': 4.638567909664465,
+  },
+  
+}
