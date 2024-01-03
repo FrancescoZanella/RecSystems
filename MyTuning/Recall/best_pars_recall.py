c5c2846a1885cfaac1d0e5f2e4672662ac6b960e
FrancescoZanella <francescozanella00@gmail.com>
2024-01-03 12:13:44 +0100

    edit best recall

--- MyTuning/Recall/best_pars_recall.py
+++ MyTuning/Recall/best_pars_recall.py
@@ -5,26 +5,26 @@ best_pars_recall_dict = {
     'alpha': 0.0032698816134106856
   },
   'RP3betaRecommender': {
-    'topK':55,
-    'alpha': 0.251168127955914,
-    'beta': 0.151393522942823,
+    'topK':66,
+    'alpha': 0.30077950803045783,
+    'beta': 0.22779750090858622,
     'normalize_similarity':True,
   },
   'EASE_R_Recommender': {
-    'topK': 1339, 
-    'l2_norm': 227.09374680918643
+    'topK': 300, 
+    'l2_norm': 78.48190404414783
   },
   'P3alphaRecommender': {
-    'topK': 143,
+    'topK': 180,
     'alpha': 0.471097731898723,
     'normalize_similarity': True,
   },
   'ItemKNNCFRecommender': {
     'similarity': "tversky",
-    'topK': 18,
-    'shrink': 47,
-    'tversky_alpha': 0.508601988213229,
-    'tversky_beta': 4.638567909664465,
+    'topK': 25,
+    'shrink': 51,
+    'tversky_alpha': 0.8124063348128021,
+    'tversky_beta': 6.7297971145367015,
   },
   
 }
