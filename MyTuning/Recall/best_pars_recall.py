f77afabd1deccc1f9fb01eeb343dccfb740e8f5b
FrancescoZanella <francescozanella00@gmail.com>
2024-01-10 20:35:15 +0100

    FINEEEEEEEEE

--- MyTuning/Recall/best_pars_recall.py
+++ MyTuning/Recall/best_pars_recall.py
@@ -5,18 +5,18 @@ best_pars_recall_dict = {
     'alpha': 0.026107565357229457
   },
   'RP3betaRecommender': {
-      'topK': 376,
-      'alpha': 0.31149747904824837,
-      'beta': 0.28155416772769626,
-      'normalize_similarity': True
+    'topK': 382,
+    'alpha': 0.315418337663744,
+    'beta': 0.2785078064899183,
+    'normalize_similarity': True
   },
   'EASE_R_Recommender': {
     'topK': 304, 
     'l2_norm': 505.36927044973623
   },
   'P3alphaRecommender': {
-    'topK': 1000, 
-    'alpha': 0.3879460479349395,
+    'topK': 1148,
+    'alpha': 0.3747336360484326,
     'normalize_similarity': True
   },
   'ItemKNNCFRecommender': {
